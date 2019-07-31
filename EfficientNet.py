b1074ff2f6b29f515e0bc2dfba59c58343030457
Gwangs <hongzolv@gmail.com>
2019-07-31 23:35:59 +0900

    no message

---
+++ EfficientNet.py
@@ -0,0 +1,16 @@
+
+
+def efficientnet_params(model_name):
+  """Get efficientnet params based on model name."""
+  params_dict = {
+      # (width_coefficient, depth_coefficient, resolution, dropout_rate)
+      'efficientnet-b0': (1.0, 1.0, 224, 0.2),
+      'efficientnet-b1': (1.0, 1.1, 240, 0.2),
+      'efficientnet-b2': (1.1, 1.2, 260, 0.3),
+      'efficientnet-b3': (1.2, 1.4, 300, 0.3),
+      'efficientnet-b4': (1.4, 1.8, 380, 0.4),
+      'efficientnet-b5': (1.6, 2.2, 456, 0.4),
+      'efficientnet-b6': (1.8, 2.6, 528, 0.5),
+      'efficientnet-b7': (2.0, 3.1, 600, 0.5),
+  }
+  return params_dict[model_name]
